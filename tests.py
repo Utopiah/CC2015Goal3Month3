36e0b75dc1a8e719d0ff3d7344df23eff69664b9
Fabien Benetou <fabien-services@benetou.fr>
2015-03-13 09:12:11 +0100

    Module with test suite with only function namesw

---
+++ tests.py
@@ -0,0 +1,43 @@
+""" Testing the MixSongsForFocus module """
+import unittest
+from MixSongsForFocus import *
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    # CREATION CLASS?
+    def test_load_past_creations(self):
+        raise NotImplementedError
+
+    def test_make_new_creations(self):
+        raise NotImplementedError
+
+    def test_creation_respect_structure(self):
+        raise NotImplementedError
+
+    def test_save_new_creations(self):
+        raise NotImplementedError
+
+   # LIBRARY CLASS?
+    def test_load_items_from_library(self):
+        raise NotImplementedError
+
+    def test_extend_library(self):
+        raise NotImplementedError
+
+    def test_blend_items_from_library(self):
+        raise NotImplementedError
+
+    def test_find_similar_item_from_library(self):
+        raise NotImplementedError
+
+    def test_modify_item_from_library(self):
+        raise NotImplementedError
+
+    def test_save_items_to_library(self):
+        raise NotImplementedError
+
+if __name__ == '__main__':
+    unittest.main()
